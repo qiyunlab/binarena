de86e3e007761aa6a32d24c30fc5c8da12977a69
ziw <zijnuw@gmail.com>
2020-07-31 00:47:28 -0500

    updated coding style

--- static/js/ari.js
+++ static/js/ari.js
@@ -21,16 +21,16 @@ function arrSum(arr) {
  * @return {number} the factorial quotient of 2 integers
  */
 function factorial(n, m=1) {
-	if (!Number.isInteger(n) || (m != undefined && !Number.isInteger(m))) {
-		return 0;
-	}
-	let res = m;
-	let i = m;
-	while (i < n) {
-		res *= i + 1;
-		i++;
-	}
-	return res;
+  if (!Number.isInteger(n) || (m != undefined && !Number.isInteger(m))) {
+    return 0;
+  }
+  let res = m;
+  let i = m;
+  while (i < n) {
+    res *= i + 1;
+    i++;
+  }
+  return res;
 }
 
 
@@ -42,10 +42,10 @@ function factorial(n, m=1) {
  * @return {number} the combination of n choose m
  */
 function comb(n, m) {
-	if (!Number.isInteger(n) || (m != undefined && !Number.isInteger(m))) {
-		return 0;
-	}
-	return factorial(n, n - m + 1) / factorial(m);
+  if (!Number.isInteger(n) || (m != undefined && !Number.isInteger(m))) {
+    return 0;
+  }
+  return factorial(n, n - m + 1) / factorial(m);
 }
 
 
@@ -57,20 +57,20 @@ function comb(n, m) {
  * @return {number[]} the unique array and optional indices of unique array
  */
 function unique(arr, returnInv=false) {
-	let res = Array.from(new Set(arr));
-	if (!returnInv) {
-		return res;
-	} else {
-		let inv = Array(arr.length).fill();
-		for (let i = 0; i < arr.length; i++) {
-			for (let j = 0; j < res.length; j++) {
-				if (arr[i] === res[j]) {
-					inv[i] = j;
-				}
-			}
-		}
-		return [res, inv];
-	}
+  let res = Array.from(new Set(arr));
+  if (!returnInv) {
+    return res;
+  } else {
+    let inv = Array(arr.length).fill();
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < res.length; j++) {
+        if (arr[i] === res[j]) {
+          inv[i] = j;
+        }
+      }
+    }
+    return [res, inv];
+  }
 }
 
 
@@ -85,24 +85,24 @@ function unique(arr, returnInv=false) {
  * @return {number[]} the coordinate matrix 
  */
 function coordinateMatrix(row, col, data, shape, sparse=false) {
-	let res = Array(shape[0]).fill().map(()=>Array(shape[1]).fill(0));
-	for (let i = 0; i < row.length; i++) {
-		res[row[i]][col[i]] += data[i];
-	}	
-	if (!sparse) {
-		return res;
-	} else {
-		let key = [], value = [];
-		for (let i = 0; i < res[0].length; i++) {
-			for (let j = 0; j < res[1].length;      j ++) {
-				if (res[i][j] !== 0) {
-					key.push([i, j]);
-					value.push(res[i][j]);
-				}
-			}
-		}
-		return [key, value]
-	}
+  let res = Array(shape[0]).fill().map(()=>Array(shape[1]).fill(0));
+  for (let i = 0; i < row.length; i++) {
+    res[row[i]][col[i]] += data[i];
+  }  
+  if (!sparse) {
+    return res;
+  } else {
+    let key = [], value = [];
+    for (let i = 0; i < res[0].length; i++) {
+      for (let j = 0; j < res[1].length;      j ++) {
+        if (res[i][j] !== 0) {
+          key.push([i, j]);
+          value.push(res[i][j]);
+        }
+      }
+    }
+    return [key, value]
+  }
 }
 
 /**
@@ -113,12 +113,12 @@ function coordinateMatrix(row, col, data, shape, sparse=false) {
  * @return {number[]} the contingency matrix
  */
 function contingencyMatrix(labelTrue, labelPred) {
-	let [classes, classIdx] = unique(labelTrue, true);
-	let [clusters, clusterIdx] = unique(labelPred, true);
-	let nClasses = classes.length;
-	let nClusters = clusters.length;
-	let contingency = coordinateMatrix(classIdx, clusterIdx, Array(classIdx.length).fill(1), [nClasses, nClusters], true);
-	return contingency;
+  let [classes, classIdx] = unique(labelTrue, true);
+  let [clusters, clusterIdx] = unique(labelPred, true);
+  let nClasses = classes.length;
+  let nClusters = clusters.length;
+  let contingency = coordinateMatrix(classIdx, clusterIdx, Array(classIdx.length).fill(1), [nClasses, nClusters], true);
+  return contingency;
 }
 
 
@@ -130,49 +130,39 @@ function contingencyMatrix(labelTrue, labelPred) {
  * @return {number} adjusted rand score
  */ 
 function adjustedRandScore(labelTrue, labelPred) {
-	let nSamples = labelTrue.length;
-	let nClasses = unique(labelTrue).length;
-	let nClusters = unique(labelPred).length;
-	console.log('n_sample: ' + nSamples + '\n n_classes: ' + nClasses + '\n n_clusters: ' + nClusters);
+  let nSamples = labelTrue.length;
+  let nClasses = unique(labelTrue).length;
+  let nClusters = unique(labelPred).length;
+  console.log('n_sample: ' + nSamples + '\n n_classes: ' + nClasses + '\n n_clusters: ' + nClusters);
 
-	if (nClasses == nClusters === 1 || nClasses == nClusters === 0 || nClasses === nClusters === nSamples) {
-		console.log('true')
-		return 1.0;
-	}
+  if (nClasses == nClusters === 1 || nClasses == nClusters === 0 || nClasses === nClusters === nSamples) {
+    console.log('true')
+    return 1.0;
+  }
 
-	let contingency = contingencyMatrix(labelTrue, labelPred);
-	let classSum = Array(nClasses).fill(0);
-	let clusterSum = Array(nClusters).fill(0);
-	for (let i = 0; i < contingency[0].length; i++) {
-		classSum[contingency[0][i][0]] += contingency[1][i];
-		clusterSum[contingency[0][i][1]] += contingency[1][i];
-	}
+  let contingency = contingencyMatrix(labelTrue, labelPred);
+  let classSum = Array(nClasses).fill(0);
+  let clusterSum = Array(nClusters).fill(0);
+  for (let i = 0; i < contingency[0].length; i++) {
+    classSum[contingency[0][i][0]] += contingency[1][i];
+    clusterSum[contingency[0][i][1]] += contingency[1][i];
+  }
 
-	/*
-	// list comprehension is somehow broken
-	let sumCombK = arrSum([for (nK of classSum) comb(nK, 2)]);
-	let sumCombC = arrSum([for (nC of clusterSum) comb(nC, 2)]);
-	let sumComb = arrSum([for (nIJ of contingency[1]) comb(nIJ, 2)]);
-	*/
+  let combK = classSum.map(x => comb(x, 2));
+  let combC = clusterSum.map(x => comb(x, 2));
+  let combIJ = contingency[1].map(x => comb(x, 2));
 
+  let sumCombK = arrSum(combK);
+  let sumCombC = arrSum(combC);
+  let sumComb = arrSum(combIJ);
+  let prodComb = sumCombK * sumCombC / comb(nSamples, 2);
+  let meanComb = (sumCombK + sumCombC) / 2;
 
-	let combK = classSum.map(x => comb(x, 2));
-	let combC = clusterSum.map(x => comb(x, 2));
-	let combIJ = contingency[1].map(x => comb(x, 2));
-	
-	console.log(contingency)
-	console.log(classSum)
-	console.log('combK is: ' + combK)
-	let sumCombK = arrSum(combK);
-	let sumCombC = arrSum(combC);
-	let sumComb = arrSum(combIJ);
-	let prodComb = sumCombK * sumCombC / comb(nSamples, 2);
-	let meanComb = (sumCombK + sumCombC) / 2;
-
-	return (sumComb - prodComb) / (meanComb - prodComb);
+  return (sumComb - prodComb) / (meanComb - prodComb);
 }
 
-
+/*
 let true_label = [0,0,1,0,2];
 let pred_label = [0,1,1,0,2];
 console.log(adjustedRandScore(true_label, pred_label))
+*/
